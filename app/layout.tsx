6e17528b6f86356208bdde0017f9c8fc653c9d86
vitthal-bhandari <vitthalbhandari98@gmail.com>
2024-02-20 05:46:24 +0530

    add og headers for twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,15 @@ export const metadata: Metadata = {
     url: 'https://minimalistic-tile-matching-game.vercel.app/',
     images: '/opengraph-image.jpg',
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Minimalistic Tile Matching Game',
+    description: 'A minimalistic 30-second tile matching game made for fun',
+    siteId: '1411180751966543876',
+    creator: '@Vit_Bhandari',
+    creatorId: '1411180751966543876',
+    images: ['https://minimalistic-tile-matching-game.vercel.app/opengraph-image.jpg'], // Must be an absolute URL
+  },
 }
 
 export default function RootLayout({
